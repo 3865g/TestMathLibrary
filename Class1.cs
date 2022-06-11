7cd81a60296d28a30a4cdbd803042ffb5b0aecb5
George <3865georg@gmail.com>
2022-06-11 14:51:37 +0300

    init

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestMathLibrary;
+
+public static class TestMathLibrary
+{
+    public static double Add(double firstValue, double secondValue)
+    {
+        return firstValue + secondValue;
+    }
+        public static double Subtract(double firstValue, double secondValue)
+    {
+        return firstValue - secondValue;
+    }
+        public static double Multiply(double firstValue, double secondValue)
+    {
+        return firstValue * secondValue;
+    }
+        public static double Divide(double firstValue, double secondValue)
+    {
+        if(secondValue == 0)
+            return 0;
+        return firstValue / secondValue;
+    }
+}
+    
+
+
+
+
